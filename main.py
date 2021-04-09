f9da18086b3dd01b62d8856e895934bd580e55e1
dhrgt <d27gupta@edu.uwaterloo.ca>
2021-04-09 14:56:05 -0500

    finish tutorial

--- main.py
+++ main.py
@@ -10,18 +10,21 @@ app = Flask(__name__) #common, you do this everytime you create a flask app, lik
 api = Api(app) #wrap our app in an API, initializes the fact that we are using a RESTful API
 
 #create names object of type dictionary
+names = {"Dhruv": {"age": 25, "shoots": "left"},
+            "Chanakya": {"age": 32, "shoots": "right"}}
+
 
 ##step3
 #create class with whatever information we want to return
 class HelloWorld(Resource): #create a resource for HelloWorld
-    def get(self, name, age): #GET method request, this is what will happen when a GET request is sent to a certain URL
-        return {"name":name, "age":age} #this is a dictionary, return this info in response to a GET request
+    def get(self, name): #GET method request, this is what will happen when a GET request is sent to a certain URL
+        return names[name] #this is a dictionary, return this info in response to a GET request
 
     # def post(self): #POST method request
     #     return {"Dhruv": "Posted"} #return when POST request is made
 
 #register above as a Resource
-api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:age>") #since HellowWorld(Resource) is a resource lets add to API. So if we sent a GET request to /helloworld, it should return "Hello World"
+api.add_resource(HelloWorld, "/helloworld/<string:name>") #since HellowWorld(Resource) is a resource lets add to API. So if we sent a GET request to /helloworld, it should return "Hello World"
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ main_test.py
@@ -0,0 +1,6 @@
+import requests
+
+BASE = "http://127.0.0.1:5000/"
+
+response = requests.get(BASE + "helloworld/Chanakya") #concatenate http URL with the helloworld resource so it will look like https://127.0.0.1:5000/helloworld
+print(response.json()) #print response in a JSON format by calling .json() method
