0da2a2f8980c7779b04195189018c1ec5d387b42
dhrgt <d27gupta@edu.uwaterloo.ca>
2021-04-09 23:36:22 -0500

    handling delete requests

--- video.py
+++ video.py
@@ -1,21 +1,44 @@
 from flask import Flask, request #request handles data retrieval for PUT method
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
 videos_put_args = reqparse.RequestParser() #make a new request parser object and automatically parse through request that we make
-videos_put_args.add_argument("name", type=str, help="Name of the video")
-videos_put_args.add_argument("views", type=int, help="Views of the video")
-videos_put_args.add_argument("likes", type=int, help="Likes of the video")
+videos_put_args.add_argument("name", type=str, help="Name of the video", required=True)
+videos_put_args.add_argument("views", type=int, help="Views of the video", required=True)
+videos_put_args.add_argument("likes", type=int, help="Likes of the video", required=True)
+
+videos = {} #initializer of videos dictionary
+
+#define function to handle scenarios where there is not data for specified video_id
+def abort_if_video_id_not_exist(video_id):
+    if video_id not in videos:
+        abort(404, message ="Video id is not valid!") #need status code, and message = args
+
+def abort_if_video_id_exist(video_id):
+     if video_id in videos:
+         abort(409, message ="Video already exists for that video_id!")
 
 class Video(Resource):
+
+    #return
     def get(self, video_id): #GET method where we pass video_id
+        abort_if_video_id_not_exist(video_id) #run abort function if video_id does not exist
         return videos[video_id]
 
+    #create
     def put(self, video_id): #PUT method to put information for each video_id
-        print(request.form)
-        return {}
+        abort_if_video_id_exist(video_id)
+        videos[video_id] = videos_put_args.parse_args()
+        return videos[video_id], 201
+
+    #delete
+    def delete(self, video_id):
+        abort_if_video_id_not_exist(video_id)
+        del videos[video_id] #delete the video_id in the request
+        return '', 204 #204 means deleted successfully
+
 
 api.add_resource(Video, "/video/<int:video_id>") #send information to this request
 
--- video_test.py
+++ video_test.py
@@ -2,5 +2,18 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes":10 }) #concatenate http URL with the helloworld resource so it will look like https://127.0.0.1:5000/helloworld
-print(response.json()) #print response in a JSON format by calling .json() method
+data = [{"name": "How to cut an avocado", "views":40000, "likes":100000},
+        {"name": "The story of risk", "views":200000, "likes": 180000},
+        {"name": "The rise of Dhruv", "views":300000, "likes": 300000}]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i]) #concatenate http URL with the helloworld resource so it will look like https://127.0.0.1:5000/helloworld
+    print(response.json()) #print response in a JSON format by calling .json() method
+
+input()
+response = requests.delete(BASE + "video/0") #delete first video (index = 0)
+print(response)
+
+input()
+response = requests.get(BASE + "video/1")
+print(response.json())
